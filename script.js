43bf8d157d94cd1a737b25a302d0c5a884dcc62d
PhoenixGYT <daraosunkoya@gmail.com>
2025-06-03 17:49:41 -0500

    Added playRound logic comparing User input and computer choice and point reward system.

--- script.js
+++ script.js
@@ -5,19 +5,53 @@ let computerScore = 0;
 function getComputerChoice (){
     let roll = Math.random(3);
     if (roll == 1){
-        return "Rock";
+        return "ROCK";
     }
     if (roll == 2){
-        return "Paper";
+        return "PAPER";
     }
     if (roll == 3){
-        return "Scissors";
+        return "SCISSORS";
     }
 }
 
 function getHumanChoice (){
     let msg = "Rock | Paper | Scissors";
     let choice = prompt(msg);
+    choice.toUpperCase;
+}
+
+function playRound(){
+    let humanChoice = getHumanChoice(); 
+    let computerChoice = getComputerChoice();
+
+    if (humanChoice == "ROCK" && computerChoice == "SCISSORS"){
+        console.log("You Win! Computer choose SCISSORS");
+        humanScore ++;
+    }
+    if (humanChoice == "SCISSORS" && computerChoice == "PAPER"){
+        console.log("You Win! Computer choose PAPER");
+        humanScore ++;
+    }
+    if (humanChoice == "PAPER" && computerChoice == "ROCK"){
+        console.log("You Win! Computer choose ROCK");
+        humanScore ++;
+    }
+    if (computerChoice == "ROCK" && humanChoice == "SCISSORS"){
+        console.log("You Lose! Computer choose ROCK");
+        computerScore ++;
+    }
+    if (computerChoice == "SCISSORS" && humanChoice == "PAPER"){
+        console.log("You Lose! Computer choose SCISSORS");
+        computerScore ++;
+    }
+    if (computerChoice == "PAPER" && computerhumanChoiceChoice == "ROCK"){
+        console.log("You Lose! Computer choose PAPER");
+        computerScore ++;
+    }
+    if (computerChoice === humanChoice){
+        console.log("It's a Tie!");
+    }
 }
 
 console.log(getHumanChoice());
